c4124004ea8c8a1347c4e2dec8c150f1fb7a6f91
alouilouai <louaialoui1993@gmail.com>
2025-01-08 14:06:17 +0100

    adding navbar component + making the background color to black

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center py-4 px-6">
+      <div className="flex items-center">
+        <Image
+          src="/placeholder.svg?height=40&width=120"
+          alt="FrameProd Logo"
+          width={120}
+          height={40}
+          className="mr-4"
+        />
+      </div>
+      <ul className="flex space-x-6">
+        {["Home", "Services", "Portfolio", "About", "Contact"].map((item) => (
+          <li key={item}>
+            <Link
+              href={`#${item.toLowerCase()}`}
+              className="text-orange-500 hover:text-orange-300 transition-colors"
+            >
+              {item}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
